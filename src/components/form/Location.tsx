3f9c29cb1ddef79ebb700b314070f80f8cf31558
Vanit Yadav <110538930+vanityadav@users.noreply.github.com>
2023-08-10 01:29:13 +0530

    Push

--- src/components/form/Location.tsx
+++ src/components/form/Location.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 export default function Location() {
   const geoApi = useGeoLocationApi();
-  console.log(geoApi);
 
   if (geoApi.location)
     return (
--- src/components/form/Webcam.tsx
+++ src/components/form/Webcam.tsx
@@ -8,21 +8,38 @@ const videoConstraints = {
 };
 
 const WebcamCapture = () => {
+  const [deviceId, setDeviceId] = React.useState({});
+  const [devices, setDevices] = React.useState([]);
+
+  const handleDevices = React.useCallback(
+    (mediaDevices) =>
+      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
+    [setDevices]
+  );
+
+  React.useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(handleDevices);
+  }, [handleDevices]);
+  const [imageSrc, setImageSrc] = React.useState();
+
   const webcamRef = React.useRef(null);
   const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
+    setImageSrc(webcamRef.current.getScreenshot());
   }, [webcamRef]);
   return (
     <>
       <Webcam
-        audio={false}
+        className=""
         height={720}
         ref={webcamRef}
-        screenshotFormat="image/jpeg"
         width={1280}
+        onUserMediaError={() => console.log("No camera")}
         videoConstraints={videoConstraints}
       />
       <button onClick={capture}>Capture photo</button>
+      <div>
+        <img src={imageSrc} />
+      </div>
     </>
   );
 };
